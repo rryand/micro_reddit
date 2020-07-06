bf8601e60bccb1502ef1f13d50ed295af7b697a3
Ramses Ryan Dineros <ramsesryandineros@gmail.com>
2020-07-06 21:56:43 +0800

    added User and Post models

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  
+  validates :title, :user_id, presence: true
+  validates :body, presence: true, if: -> { link.blank? }
+  validates :link, presence: true, if: -> { body.blank? }
+  validate :body_and_link_cannot_be_both_present
+
+  private
+
+  def body_and_link_cannot_be_both_present
+    if body.present? && link.present?
+      errors.add(:base, "body and link can't both be present")
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  
+  validates :username, length: { in: 4..12 }, uniqueness: true, presence: true
+  validates :password, length: { in: 6..12 }, presence:true
+end
